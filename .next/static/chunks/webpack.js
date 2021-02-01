d8ac438f88a60ca086ec59ed7d79608fe8a144f1
Roger <williroger@usp.br>
2021-01-31 23:55:41 -0300

    testando 01

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2a64f76ca7f37b659acf";
+/******/ 	var hotCurrentHash = "86b629d3cbb4fa72ef53";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
